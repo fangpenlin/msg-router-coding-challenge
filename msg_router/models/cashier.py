c8181f113e738eddeec943ddb9e5f9042510fa67
Victor Lin <bornstub@gmail.com>
2015-02-09 21:40:51 -0800

    Implement cashier algorithm

--- msg_router/models/cashier.py
+++ msg_router/models/cashier.py
@@ -2,10 +2,22 @@ from __future__ import unicode_literals
 
 
 class Cashier(object):
+    """Greedy coin change problem solver.
+
+    """
 
     def __init__(self, denominations):
         self.denominations = denominations
 
     def __call__(self, value):
-        # TODO: implement greedy cashier coin change algorithm
-        pass
+        result = {}
+        remain = value
+        for denomination in reversed(self.denominations):
+            if not remain:
+                break
+            coin_count = remain / denomination
+            if not coin_count:
+                continue
+            result[denomination] = coin_count
+            remain = remain % denomination
+        return result
