5b28a9f9b997350724ce19105bf2eb766a1ab3b9
Victor Lin <bornstub@gmail.com>
2015-02-09 23:24:22 -0800

    Fix broken round robin testing

--- msg_router/api/route_msg/controllers.py
+++ msg_router/api/route_msg/controllers.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+import itertools
 
 from netaddr import IPNetwork
 from pyramid.view import view_config
@@ -15,34 +16,41 @@ class RouteMessageController(ControllerBase):
     @view_config(request_method='POST')
     def post(self):
         routing_table = self.settings['routing.table']
-        denominations = map(lambda item: item[0], routing_table)
-        denomination_to_cidr = dict(routing_table)
+        # map throughput to cidr, like 5 to 10.0.2.0/24
+        throughput_to_cidr = dict(routing_table)
+        # all throughputs in routing table
+        throughputs = throughput_to_cidr.keys()
+
+        # map denomination (throughput) to ip address generators
+        throughput_to_ip_geneator = {}
+        for throughput, cidr in throughput_to_cidr.iteritems():
+            # the pool of ip addresses in the cidr
+            # except broadcasting address, the .0 one
+            ip_pool = list(IPNetwork(cidr))[1:]
+            throughput_to_ip_geneator[throughput] = itertools.cycle(ip_pool)
 
         message = self.request.json['message']
         recipients = self.request.json['recipients']
         # TODO: validate recipients
 
         # use cachier algorithm to solve the problem
-        cashier = models.Cashier(denominations)
+        cashier = models.Cashier(throughputs)
         solution = cashier(len(recipients))
 
         routes = []
-        result_denominations = sorted(solution.keys())
-        for denomination in result_denominations:
-            # count of requests for the denomination (routing)
-            req_count = solution[denomination]
+        result_throughputs = sorted(solution.keys())
+        for throughput in result_throughputs:
+            # total count of requests for the throughput (routing)
+            req_count = solution[throughput]
             for _ in xrange(req_count):
-                cidr = denomination_to_cidr[denomination]
-                network = IPNetwork(cidr)
-                # TODO: use a round-robin manner to get an IP address from the
-                # subnet?
-                ip = str(network[1])
+                gen_ip = throughput_to_ip_geneator[throughput]
+                ip = str(gen_ip.next())
 
                 routes.append(dict(
                     ip=ip,
-                    recipients=recipients[:denomination],
+                    recipients=recipients[:throughput],
                 ))
-                recipients = recipients[denomination:]
+                recipients = recipients[throughput:]
 
         return dict(
             message=message,
